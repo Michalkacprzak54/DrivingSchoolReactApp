5642dc0ec7a91816a9ab7ff24b759ec12090a97e
Michał Kacprzak <michalkacprzak54@wp.pl>
2024-12-20 11:00:03 +0100

    +added distinctied forms for course service

--- drivingschoolreactapp.client/src/forAll/serviceDetails.jsx
+++ drivingschoolreactapp.client/src/forAll/serviceDetails.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import { createAPIEndpoint, ENDPOINTS } from '../api/index';
 import { addToCart } from './cart/cartUtils';
 
-
 function ServiceDetailPage() {
     const { idService } = useParams(); // Pobranie idService z URL
     const [service, setService] = useState(null);
@@ -14,8 +13,8 @@ function ServiceDetailPage() {
     const [formData, setFormData] = useState({
         theoryStatus: "",
         practiceType: "",
+        serviceOption: "",
     });
-
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -29,14 +28,17 @@ function ServiceDetailPage() {
         e.preventDefault();
 
         const newErrors = {};
-        if (formData.theoryStatus === "") newErrors.theoryStatus = 'Proszę wybrać formę praktyki.';
-        if (formData.practiceType === "") newErrors.practiceType = 'Proszę wybrać typ praktyki.';
+        if (service.serviceType === "Kurs") {
+            if (formData.theoryStatus === "") newErrors.theoryStatus = 'Proszę wybrać formę praktyki.';
+            if (formData.practiceType === "") newErrors.practiceType = 'Proszę wybrać typ praktyki.';
+        } else if (service.serviceType === "Usługa") {
+            if (formData.serviceOption === "") newErrors.serviceOption = 'Proszę wybrać opcję usługi.';
+        }
 
         if (Object.keys(newErrors).length > 0) {
             setErrors(newErrors);
             return;
         }
-
 
         setErrors({});
         console.log('Form submitted:', formData);
@@ -60,8 +62,7 @@ function ServiceDetailPage() {
 
     useEffect(() => {
         fetchServiceDetails();
-        
-    }, [idService]); 
+    }, [idService]);
 
     return (
         <div className="service-detail-page">
@@ -97,51 +98,75 @@ function ServiceDetailPage() {
                         <p>Brak zdjęć dla tej usługi.</p>
                     )}
 
-                    {/* Formularz z wyborem opcji */}
-                    <form onSubmit={handleSubmit} className="service-form">
-                        <div className="input-group">
-                            <label htmlFor="theoryStatus" className="input-label">Wybierz formę praktyki</label>
-                            <select
-                                id="theoryStatus"
-                                name="theoryStatus"
-                                value={formData.theoryMode}
-                                onChange={handleChange}
-                                className="input-field"
-                                required 
-                            >
-                                <option value="">Wybierz...</option>
-                                <option value="stacjonarna">Stacjonarna</option>
-                                <option value="online">Online</option>
-                                <option value="zaliczona">Zaliczona</option>
-                            </select>
-                            {errors.theoryStatus && <p className="error-message">{errors.theoryMode}</p>}
-                        </div>
+                    {/* Renderowanie odpowiedniego formularza */}
+                    {service.serviceType === "Kurs" ? (
+                        <form onSubmit={handleSubmit} className="service-form">
+                            <div className="input-group">
+                                <label htmlFor="theoryStatus" className="input-label">Wybierz formę praktyki</label>
+                                <select
+                                    id="theoryStatus"
+                                    name="theoryStatus"
+                                    value={formData.theoryStatus}
+                                    onChange={handleChange}
+                                    className="input-field"
+                                >
+                                    <option value="">Wybierz...</option>
+                                    <option value="stacjonarna">Stacjonarna</option>
+                                    <option value="online">Online</option>
+                                    <option value="zaliczona">Zaliczona</option>
+                                </select>
+                                {errors.theoryStatus && <p className="error-message">{errors.theoryStatus}</p>}
+                            </div>
 
-                        <div className="input-group">
-                            <label htmlFor="practiceType" className="input-label">Typ praktyki</label>
-                            <select
-                                id="practiceType"
-                                name="practiceType"
-                                value={formData.practiceType}
-                                onChange={handleChange}
-                                className="input-field"
-                                required 
-                            >
-                                <option value="">Wybierz...</option>
-                                <option value="podstawowa">Podstawowa - 30h</option>
-                                <option value="rozszerzona">Rozszerzona - 40h</option>
-                            </select>
-                            {errors.practiceType && <p className="error-message">{errors.practiceType}</p>}
-                        </div>
+                            <div className="input-group">
+                                <label htmlFor="practiceType" className="input-label">Typ praktyki</label>
+                                <select
+                                    id="practiceType"
+                                    name="practiceType"
+                                    value={formData.practiceType}
+                                    onChange={handleChange}
+                                    className="input-field"
+                                >
+                                    <option value="">Wybierz...</option>
+                                    <option value="podstawowa">Podstawowa - 30h</option>
+                                    <option value="rozszerzona">Rozszerzona - 40h</option>
+                                </select>
+                                {errors.practiceType && <p className="error-message">{errors.practiceType}</p>}
+                            </div>
 
-                        <button type="submit" className="add-to-cart-button">
-                            Dodaj do koszyka
-                        </button>
-                    </form>
+                            <button type="submit" className="add-to-cart-button">
+                                Dodaj do koszyka
+                            </button>
+                        </form>
+                    ) : service.serviceType === "Usługa" ? (
+                        <form onSubmit={handleSubmit} className="service-form">
+                            <div className="input-group">
+                                <label htmlFor="serviceOption" className="input-label">Opcja usługi</label>
+                                <select
+                                    id="serviceOption"
+                                    name="serviceOption"
+                                    value={formData.serviceOption}
+                                    onChange={handleChange}
+                                    className="input-field"
+                                >
+                                    <option value="">Wybierz...</option>
+                                    <option value="automat">Automat</option>
+                                    <option value="manual">Manual</option>
+                                </select>
+                                {errors.serviceOption && <p className="error-message">{errors.serviceOption}</p>}
+                            </div>
+
+                            <button type="submit" className="add-to-cart-button">
+                                Dodaj do koszyka
+                            </button>
+                        </form>
+                    ) : (
+                        <p>Nieobsługiwany typ usługi.</p>
+                    )}
                 </div>
             )}
         </div>
     );
-};
+}
 
 export default ServiceDetailPage;
